f91b5aa831d95d27b72d819ac321c691cb424acd
ronmaa <ronmaa.7@gmail.com>
2022-11-19 16:29:12 +0530

    Filtering features added

--- controller/jobsController.js
+++ controller/jobsController.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const { Op } = require("sequelize");
 const CustomAPIError = require("../errors/customAPIError");
 require("dotenv").config();
 const jobModel = require("../models/jobModel");
@@ -102,18 +103,54 @@ async function viewAllJob(req, res) {
       }
     });
   }
+
   console.log(orderingQuery);
-  const username = req.username;
+
+  let filteringQuery = {};
+  filteringQuery.userName = req.username;
+  if (req.query.filter) {
+    let filter = req.query.filter.split(",");
+    //[salary>4 , name=hello ]
+    /*
+      {
+        salary : {[op.gt] : 4},
+        name : {[op.eq] : hello}
+      }
+    */
+    const signMap = {
+      "<": Op.lt,
+      "<=": Op.lte,
+      ">": Op.gt,
+      ">=": Op.gte,
+      "=": Op.eq,
+    };
+    const signRegex = /\b(<|<=|>|>=|=)\b/;
+    filter.forEach((element) => {
+      console.log(element);
+      let parts = element.replace(signRegex, (match) => {
+        return `-${match}-`;
+      });
+
+      parts = parts.split("-");
+      let thisPropertyFilter = {};
+      console.log(parts[2]);
+
+      if (filteringQuery.hasOwnProperty(parts[0])) {
+        filteringQuery[parts[0]][signMap[parts[1]]] = parts[2];
+      } else {
+        thisPropertyFilter[signMap[parts[1]]] = parts[2];
+        filteringQuery[parts[0]] = thisPropertyFilter;
+      }
+    });
+  }
   const job = await jobModel.findAll({
-    where: {
-      username: username,
-    },
+    where: filteringQuery,
     order: orderingQuery,
   });
 
   res
     .status(200)
-    .json({ yourUsername: username, yourJobs: job, nbJobs: job.length });
+    .json({ yourUsername: req.username, yourJobs: job, nbJobs: job.length });
 }
 
 async function editJob(req, res) {
